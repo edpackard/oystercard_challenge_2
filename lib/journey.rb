c6d174721b24e8bfcaf1239e93cdb8a6e78ad328
edpackard <edpackard81@gmail.com>
2021-08-05 17:33:41 +0100

    Chapter 14 journey class - record journey in progress

---
+++ lib/journey.rb
@@ -0,0 +1,21 @@
+class Journey
+
+ def initialize(name, zone)
+   @journey = {}
+   @name = name
+   @zone = zone
+ end
+
+ def see_journey
+   @journey
+ end
+
+ def see_name
+   @name
+ end
+
+ def see_zone
+   @zone
+ end
+
+end
